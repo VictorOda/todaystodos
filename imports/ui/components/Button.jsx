b23472bcc9cae2b9c82305396bb6015dc0e71eb0
Victor Oda <victor.ra.oda@gmail.com>
2016-04-18 11:50:02 -0300

    Button layout and basic theme implemented.

--- imports/ui/components/Button.jsx
+++ imports/ui/components/Button.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 export default class Button extends Component {
     render () {
+        const buttonStyle = {
+            verticalAlign: "middle",
+            fontWeight: "bold",
+            lineHeight: "46px",
+            width: "50%",
+            margin: "0 auto",
+            textAlign: "center"
+        };
+
         return (
-            <div className="text-center">
-                <button className="button button-energized icon-right ion-happy"
+            <div className="content-block">
+                <p>
+                    <a
+                        className="button button-fill button-raised"
+                        style={ buttonStyle }
                         onClick={this.props.newDay.bind(this)}>
-                    {this.props.value }
-                </button>
+                        {this.props.value }
+                    </a>
+                </p>
             </div>
         );
     }
